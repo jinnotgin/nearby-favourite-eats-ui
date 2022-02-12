d878121eaeff1ace5ca13d7c20832f012c56c72c
Jin <me@thelinjin.com>
2022-02-12 14:38:50 +0800

    moved comment message

--- src/lib/Avatar.svelte
+++ src/lib/Avatar.svelte
@@ -2,11 +2,11 @@
 	export let uid;
 	export let size = '3rem';
 
+	// old image generated from robohash `https://robohash.org/${$auth.user.uid}.png?set=set1&size=64x64`
+	// replaced with DiceBear Avatars
 	import { createAvatar } from '@dicebear/avatars';
 	import * as style from '@dicebear/adventurer-neutral';
 
-	// old image generated from robohash `https://robohash.org/${$auth.user.uid}.png?set=set1&size=64x64`
-	// replaced with DiceBear Avatars
 	let svg = createAvatar(style, {
 		seed: uid,
 		backgroundColor: ['variant01', 'variant02', 'variant03']
